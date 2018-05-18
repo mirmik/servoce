c35bf6527b5029bc6148ce490d489c3af8eb930f
mirmik <mirmikns@yandex.ru>
2018-05-18 15:57:17 +0300

    a

---
+++ servoce_inc.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+import licant
+from licant.cxx_modules import shared_library
+from licant.modules import module, submodule
+
+import licant.libs
+import os
+
+licant.libs.include("gxx")
+
+libqt_include_path = "/usr/include/x86_64-linux-gnu/qt5/"
+liboce_include_path = "/usr/local/include/opencascade/"
+python_include_prefix = "/usr/include/"
+
+install_lib_dir = "/usr/lib/"
+install_inc_dir = "/usr/local/include/"
+
+module('liboce', 
+	libs = [     
+		'TKernel',
+		'TKMath',
+		'TKG3d',
+		'TKBRep',
+		'TKGeomBase',
+		'TKGeomAlgo',
+		'TKTopAlgo',
+		'TKPrim',
+		'TKBO',
+		'TKBool',
+		'TKOffset',
+		'TKService',
+		'TKV3d',
+		'TKOpenGl',
+		'TKFillet',
+		'TKSTL',
+		'TKBin',
+	],
+	include_paths = [liboce_include_path]    
+)
+
+module('libqt', 
+	libs = [    
+		'Qt5Core', 
+		'Qt5Widgets', 
+		'Qt5Test', 
+		'Qt5Gui', 
+		'Qt5OpenGL',
+	],
+	include_paths = [libqt_include_path]    
+)
+
+module('servoce_sources', 
+	srcdir = "src",
+	sources = [
+		"math3.cpp",
+		"topo.cpp",
+		"trans.cpp",
+		"solid.cpp",
+		"face.cpp",
+		"wire.cpp",
+		"boolops.cpp",
+		"display.cpp",
+		"test.cpp",
+		"convert.cpp",
+
+		"display/mainwidget.cpp",
+		"display/dispwidget.cpp",
+		"display/dispwidget_qt.cpp",
+	],
+	moc = ["local/display.h"],  
+	include_paths = [ "include", ".", "src" ],
+	include_modules = [
+		submodule("liboce"), 
+		submodule("libqt"), 
+		submodule("gxx.include"),
+		submodule("gxx", "posix"),
+		submodule("gxx.print", "cout"),
+		submodule("gxx.dprint", "cout"),
+	],
+	cxx_flags = '-fPIC -DQT_NO_VERSION_TAGGING',
+	cc_flags = '-fPIC',
+)
+
