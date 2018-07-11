0121d4c01c58173241835776effc47aae03a2fc4
mirmik <mirmikns@yandex.ru>
2018-07-11 12:52:51 +0300

    a

--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ pyservoce_lib = Extension("pyservoce.libservoce",
 setup(
 	name = 'pyservoce',
 	packages = ['pyservoce'],
-	version = '0.0.1',
+	version = '1.0.0',
 	license='MIT',
 	description = 'CAD system for righteous zen programmers ',
 	author = 'Sorokin Nikolay',
