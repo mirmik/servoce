deaba9a345bf3ca3be54e80ca0c86472f12e91ab
mirmik <mirmikns@yandex.ru>
2018-12-02 21:26:58 +0300

    index current state as release

--- tools/build_dists.sh
+++ tools/build_dists.sh
@@ -1,5 +1,4 @@
-#python3.6 ./setup.py bdist_wheel
-python3.5 ./setup.py bdist_wheel
+#python3.5 ./setup.py bdist_wheel
 python3.6 ./setup.py bdist_wheel
 python3.7 ./setup.py bdist_wheel
 python2.7 ./setup.py bdist_wheel
