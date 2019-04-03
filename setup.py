3d82f3dbe7aceb05a48c988dc7df85e6db30a10b
mirmik <mirmikns@yandex.ru>
2019-04-03 22:42:38 +0300

    '1.9.5'

--- setup.py
+++ setup.py
@@ -87,7 +87,7 @@ pyservoce_lib = Extension("pyservoce.libservoce",
 setup(
 	name = 'pyservoce',
 	packages = ['pyservoce'],
-	version = '1.9.4',
+	version = '1.9.5',
 	license='MIT',
 	description = 'CAD system for righteous zen programmers ',
 	author = 'mirmik',
