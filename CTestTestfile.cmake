aefaba1cecf0478ccc4a1215ab317302d55279b8
mirmik <mirmikns@yandex.ru>
2019-11-14 00:28:41 +0300

    'travis'

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mirmik/project/servoce
-# Build directory: /home/mirmik/project/servoce
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(servoce_test "/home/mirmik/project/servoce/servoce_test")
