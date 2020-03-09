ee1a3e9b544ec80c7e8f3984d95a15afc2a6e02a
mirmik <mirmikns@yandex.ru>
2020-03-09 14:28:17 +0300

    'fix cmake and tests'

--- utests/curve3.cpp
+++ utests/curve3.cpp
@@ -3,16 +3,16 @@
 
 TEST(api, Curve3) 
 {
-	servoce::curve3::line({0,0,0}, {10,10,10});
+	servoce::line_curve3({0,0,0}, {10,10,10});
 
-	auto a = servoce::curve3::interpolate({{0,0,0}, {0,0,1}},{{0,0,0}, {0,0,1}},false);
-	auto b = servoce::curve3::interpolate({{0,0,0}, {0,0,1}},false);	
+	auto a = servoce::interpolate_curve3({{0,0,0}, {0,0,1}},{{0,0,0}, {0,0,1}},false);
+	auto b = servoce::interpolate_curve3({{0,0,0}, {0,0,1}},false);	
 }
 
 TEST(line, Curve3) 
 {
-	auto line = servoce::curve3::line({0,0,0}, {1,0,0});
-	auto line2 = servoce::curve3::line({0,0,0}, {10,0,0});	
+	auto line = servoce::line_curve3({0,0,0}, {1,0,0});
+	auto line2 = servoce::line_curve3({0,0,0}, {10,0,0});	
 
 	EXPECT_EQ(line.d0(0), servoce::point3(0,0,0));
 	EXPECT_EQ(line.d0(1), servoce::point3(1,0,0));
