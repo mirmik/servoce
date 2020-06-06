e122bc49ccc2ca4f46fbe9a1c86fb8a3ac34bf4f
netricks <netricks@protonmail.com>
2020-06-07 02:56:15 +0300

    'travis'

--- tools/__set_occt_paths.py
+++ tools/__set_occt_paths.py
@@ -8,17 +8,17 @@ def get_occt_include_directory():
 	if os.path.exists("./servoce-third-libs-travis/osx-include"):
 		return "./servoce-third-libs-travis/osx-include"
 
-    dirs = ["/usr/include/", "/usr/local/include/"]
-    subdirs = ["occt", "opencascade"]
+	dirs = ["/usr/include/", "/usr/local/include/"]
+	subdirs = ["occt", "opencascade"]
 
-    for d in dirs:
-        if os.path.exists(d):
-            for f in subdirs:
-                fp = os.path.join(d, f)
-                if os.path.exists(fp):
-                    return fp 
+	for d in dirs:
+		if os.path.exists(d):
+			for f in subdirs:
+				fp = os.path.join(d, f)
+				if os.path.exists(fp):
+					return fp 
 
-    raise Exception("can't find occt")
+	raise Exception("can't find occt")
 
 print("export OCCT_INCLUDE={}".format(get_occt_include_directory()))
 #print("OCCT_LIB={}", format=get_occt_include_directory())
