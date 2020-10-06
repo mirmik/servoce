39fbba643b44c65af20eae3c5c4403d9c286cf85
mirmik <sorokin@radiorf.ru>
2020-10-06 14:24:42 +0300

    build script

---
+++ tools/build_dists_win.py
@@ -0,0 +1,6 @@
+import os
+
+os.system("C:\\Users\\rfmeas\\AppData\\Local\\Programs\\Python\\Python36\\python.exe setup.py bdist_wheel")
+os.system("C:\\Users\\rfmeas\\AppData\\Local\\Programs\\Python\\Python37\\python.exe setup.py bdist_wheel")
+os.system("C:\\Users\\rfmeas\\AppData\\Local\\Programs\\Python\\Python38\\python.exe setup.py bdist_wheel")
+#os.system("C:\\Users\\rfmeas\\AppData\\Local\\Programs\\Python\\Python39\\python.exe setup.py bdist_wheel")
